55fade10eec5a8ef9b3db3f0481bdd0e2b821151
Pablo Berland <pablo.berland@epitech.eu>
2020-11-13 07:56:27 +0100

    Removing unused includes in main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -1,24 +1,15 @@
 #include "Engine/Core.hpp"
-#include "Engine/Logging/Logger.hpp"
-#include "Engine/Logging/ConsoleSink.hpp"
-#include "Engine/Logging/FileSink.hpp"
-
-#include <thread>
-#include <chrono>
-#include <iostream>
-#include <map>
 
 int main(void)
 {
     Engine::Core core("Super jeu");
 
     core.init();
-    core.init();
     core.getResourceManager()->registerResource(
         {
             Engine::ResourceType::MeshType,
             "skel",
-            "resources/skel.obj"
+            "resources/models/viking_room.obj"
         }
     );
     core.getResourceManager()->loadResources();
