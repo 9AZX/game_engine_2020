cea453dd4d43fddb4d00fcabfd77a98fc33848f8
Pablo Berland <pablo.berland@epitech.eu>
2020-11-14 16:37:28 +0100

    Adding a vulkan debug messenger
    The debug messenger uses the logger by getting it from the
    core static method getLogger(), maybe it would have been
    better to pass it as the pUserData of the debug messenger
    callback.

--- include/Engine/Graphics.hpp
+++ include/Engine/Graphics.hpp
@@ -1,6 +1,7 @@
 #ifndef ENGINE_GRAPHICS_HPP_
 #define ENGINE_GRAPHICS_HPP_
 
+#include "Engine/Graphics/DebugMessenger.hpp"
 #include "Engine/Graphics/Device.hpp"
 #include "Engine/Graphics/Instance.hpp"
 #include "Engine/Graphics/Swapchain.hpp"
@@ -12,20 +13,23 @@ namespace Engine {
 class Graphics
 {
 public:
-    Graphics(std::string name, std::shared_ptr<Engine::Window> window);
+    Graphics(
+        std::string name,
+        std::shared_ptr<Engine::Window> window,
+        bool enableDebugging
+    );
     ~Graphics();
 
     std::shared_ptr<Instance> gInstance;
     std::shared_ptr<Device> gDevice;
     std::shared_ptr<Swapchain> gSwapChain;
+    DebugMessenger messenger;
 
 private:
+    bool _enableDebugging;
     std::string _appName;
 
     std::shared_ptr<Engine::Window> _window;
-#if !defined(NDEBUG)
-    //TODO #8 Vulkan message debuging
-#endif
 }; /* class Graphics */
 
 } /* namespace Engine */
---
+++ source/Engine/Graphics/DebugMessenger.cpp
@@ -0,0 +1,110 @@
+#include "Engine/Graphics/DebugMessenger.hpp"
+
+#include "Engine/Core.hpp"
+#include "Engine/Logging/Logger.hpp"
+
+static PFN_vkCreateDebugUtilsMessengerEXT pfnVkCreateDebugUtilsMessengerEXT {};
+static PFN_vkDestroyDebugUtilsMessengerEXT pfnVkDestroyDebugUtilsMessengerEXT {};
+
+void Engine::DebugMessenger::init(std::shared_ptr<Instance> instance)
+{
+    pfnVkCreateDebugUtilsMessengerEXT
+        = reinterpret_cast<PFN_vkCreateDebugUtilsMessengerEXT>(
+            instance->getInstance()->get().getProcAddr(
+                "vkCreateDebugUtilsMessengerEXT"
+            )
+        );
+    pfnVkDestroyDebugUtilsMessengerEXT
+        = reinterpret_cast<PFN_vkDestroyDebugUtilsMessengerEXT>(
+            instance->getInstance()->get().getProcAddr(
+                "vkDestroyDebugUtilsMessengerEXT"
+            )
+        );
+
+    if (!pfnVkCreateDebugUtilsMessengerEXT
+        || !pfnVkDestroyDebugUtilsMessengerEXT) {
+        //TODO throw
+    }
+    _debugUtilsMessenger
+        = instance->getInstance()->get().createDebugUtilsMessengerEXTUnique(
+            getCreateInfo()
+        );
+}
+
+vk::DebugUtilsMessengerCreateInfoEXT Engine::DebugMessenger::getCreateInfo() noexcept
+{
+    vk::DebugUtilsMessageSeverityFlagsEXT severityFlags(
+        vk::DebugUtilsMessageSeverityFlagBitsEXT::eInfo
+        | vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning
+        | vk::DebugUtilsMessageSeverityFlagBitsEXT::eError
+    );
+
+    vk::DebugUtilsMessageTypeFlagsEXT messageTypeFlags(
+        vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral
+        | vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance
+        | vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation
+    );
+
+    return vk::DebugUtilsMessengerCreateInfoEXT(
+        {},
+        severityFlags,
+        messageTypeFlags,
+        debugMessageCallback
+    );
+}
+
+VKAPI_ATTR VkBool32 VKAPI_CALL Engine::DebugMessenger::debugMessageCallback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+    VkDebugUtilsMessageTypeFlagsEXT messageTypes,
+    const VkDebugUtilsMessengerCallbackDataEXT * pCallbackData,
+    void *
+) {
+    vk::DebugUtilsMessageSeverityFlagBitsEXT severity
+        = static_cast<vk::DebugUtilsMessageSeverityFlagBitsEXT>(messageSeverity);
+    Logging::Level loggingLevel;
+
+    if (severity & vk::DebugUtilsMessageSeverityFlagBitsEXT::eError) {
+        loggingLevel = Logging::Level::Error;
+    } else if (severity & vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning) {
+        loggingLevel = Logging::Level::Warning;
+    } else if (severity & vk::DebugUtilsMessageSeverityFlagBitsEXT::eInfo) {
+        loggingLevel = Logging::Level::Info;
+    } else if (severity & vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose) {
+        loggingLevel = Logging::Level::Debug;
+    }
+    Core::getLogger()->log(
+        loggingLevel,
+        "[Graphics] {} {} {} {}",
+        vk::to_string(static_cast<vk::DebugUtilsMessageTypeFlagsEXT>(messageTypes)),
+        pCallbackData->pMessageIdName,
+        pCallbackData->messageIdNumber,
+        pCallbackData->pMessage
+    );
+    return true;
+}
+
+VKAPI_ATTR VkResult VKAPI_CALL vkCreateDebugUtilsMessengerEXT(
+    VkInstance instance,
+    const VkDebugUtilsMessengerCreateInfoEXT * pCreateInfo,
+    const VkAllocationCallbacks * pAllocator,
+    VkDebugUtilsMessengerEXT * pMessenger
+) {
+    return pfnVkCreateDebugUtilsMessengerEXT(
+        instance,
+        pCreateInfo,
+        pAllocator,
+        pMessenger
+    );
+}
+
+VKAPI_ATTR void VKAPI_CALL vkDestroyDebugUtilsMessengerEXT(
+    VkInstance instance,
+    VkDebugUtilsMessengerEXT messenger,
+    const VkAllocationCallbacks * pAllocator
+) {
+    return pfnVkDestroyDebugUtilsMessengerEXT(
+        instance,
+        messenger,
+        pAllocator
+    );
+}
