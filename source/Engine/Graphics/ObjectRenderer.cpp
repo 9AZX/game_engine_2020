61cfd5724bd6c4c6c9098e6e5bc70852fbb4ed48
Jon <jon.i@hotmail.fr>
2020-11-15 20:02:04 +0100

    Remove useless line

--- source/Engine/Graphics/ObjectRenderer.cpp
+++ source/Engine/Graphics/ObjectRenderer.cpp
@@ -19,13 +19,9 @@ void ObjectRenderer::createObjectRenderer(Engine::Mesh modelType, Engine::Math::
 
 	descriptor.createDescriptorLayoutSetPoolAndAllocate(swapChainImageCount);
 	descriptor.populateDescriptorSets(swapChainImageCount,
-		objBuffers.uniformBuffers.get());
+									  objBuffers.uniformBuffers.get());
 
-	gPipeline.createGraphicsPipelineLayoutAndPipeline(
-		swapChainImageExtent,
-		descriptor.descriptorSetLayout,
-		Engine::Graphics::getInstance()->getRenderpass()->renderPass);
-
+	// Here register the object vertex or create pipeline or find a solution
 
 	position = _position;
 	scale = _scale;
@@ -36,24 +32,24 @@ void ObjectRenderer::draw()
 	vk::UniqueCommandBuffer cBuffer = Engine::Graphics::getInstance()->currentCommandBuffer;
 	cBuffer->bindPipeline(vk::PipelineBindPoint::eGraphics, gPipeline._graphicsPipeline.get());
 
-	vk::Buffer vertexBuffers[] = { objBuffers.vertexBuffer.get() };
-	vk::DeviceSize offsets[] = { 0 };
+	vk::Buffer vertexBuffers[] = {objBuffers.vertexBuffer.get()};
+	vk::DeviceSize offsets[] = {0};
 
 	cBuffer->bindVertexBuffers(0, 1, vertexBuffers, offsets);
 	cBuffer->bindIndexBuffer(objBuffers.indexBuffer.get(),
-		0,
-		vk::IndexType::eUint32);
+							 0,
+							 vk::IndexType::eUint32);
 
 	cBuffer->bindDescriptorSets(vk::PipelineBindPoint::eGraphics,
-		gPipeline._pipelineLayout.get(),
-		0,
-		1,
-		&descriptor.descriptorSet, 0, nullptr);
+								gPipeline._pipelineLayout.get(),
+								0,
+								1,
+								&descriptor.descriptorSet, 0, nullptr);
 	cBuffer->drawIndexed(static_cast<uint32_t>(objBuffers.indices.size()),
-		1, 
-		0, 
-		0, 
-		0);
+						 1,
+						 0,
+						 0,
+						 0);
 }
 
 Engine::Math::Matrix4 ourScale(Engine::Math::Matrix4 matrix, Engine::Math::Vector3 vector)
@@ -64,7 +60,6 @@ Engine::Math::Matrix4 ourScale(Engine::Math::Matrix4 matrix, Engine::Math::Vecto
 	Result.matrixData[1] = matrix.matrixData[1] * vector.x;
 	Result.matrixData[2] = matrix.matrixData[2] * vector.x;
 	Result.matrixData[3] = matrix.matrixData[3] * vector.x;
-
 
 	Result.matrixData[4] = matrix.matrixData[4] * vector.y;
 	Result.matrixData[5] = matrix.matrixData[5] * vector.y;
@@ -88,62 +83,13 @@ Engine::Math::Matrix4 ourTranslate(Engine::Math::Matrix4 matrix, Engine::Math::V
 {
 	Engine::Math::Matrix4 Result(matrix);
 
-	Result.matrixData[12] = matrix.matrixData[0] * vector.x 
-		+ matrix.matrixData[1] * vector.x 
-		+ matrix.matrixData[2] * vector.x 
-		+ matrix.matrixData[3] * vector.x 
-		+ matrix.matrixData[4] * vector.y
-		+ matrix.matrixData[5] * vector.y
-		+ matrix.matrixData[6] * vector.y
-		+ matrix.matrixData[7] * vector.y
-		+ matrix.matrixData[8] * vector.z
-		+ matrix.matrixData[9] * vector.z
-		+ matrix.matrixData[10] * vector.z
-		+ matrix.matrixData[11] * vector.z
-		+ matrix.matrixData[12] + matrix.matrixData[13] + matrix.matrixData[14] + matrix.matrixData[15];
+	Result.matrixData[12] = matrix.matrixData[0] * vector.x + matrix.matrixData[1] * vector.x + matrix.matrixData[2] * vector.x + matrix.matrixData[3] * vector.x + matrix.matrixData[4] * vector.y + matrix.matrixData[5] * vector.y + matrix.matrixData[6] * vector.y + matrix.matrixData[7] * vector.y + matrix.matrixData[8] * vector.z + matrix.matrixData[9] * vector.z + matrix.matrixData[10] * vector.z + matrix.matrixData[11] * vector.z + matrix.matrixData[12] + matrix.matrixData[13] + matrix.matrixData[14] + matrix.matrixData[15];
 
-	Result.matrixData[13] = matrix.matrixData[0] * vector.x
-		+ matrix.matrixData[1] * vector.x
-		+ matrix.matrixData[2] * vector.x
-		+ matrix.matrixData[3] * vector.x
-		+ matrix.matrixData[4] * vector.y
-		+ matrix.matrixData[5] * vector.y
-		+ matrix.matrixData[6] * vector.y
-		+ matrix.matrixData[7] * vector.y
-		+ matrix.matrixData[8] * vector.z
-		+ matrix.matrixData[9] * vector.z
-		+ matrix.matrixData[10] * vector.z
-		+ matrix.matrixData[11] * vector.z
-		+ matrix.matrixData[12] + matrix.matrixData[13] + matrix.matrixData[14] + matrix.matrixData[15];
+	Result.matrixData[13] = matrix.matrixData[0] * vector.x + matrix.matrixData[1] * vector.x + matrix.matrixData[2] * vector.x + matrix.matrixData[3] * vector.x + matrix.matrixData[4] * vector.y + matrix.matrixData[5] * vector.y + matrix.matrixData[6] * vector.y + matrix.matrixData[7] * vector.y + matrix.matrixData[8] * vector.z + matrix.matrixData[9] * vector.z + matrix.matrixData[10] * vector.z + matrix.matrixData[11] * vector.z + matrix.matrixData[12] + matrix.matrixData[13] + matrix.matrixData[14] + matrix.matrixData[15];
 
-	Result.matrixData[14] = matrix.matrixData[0] * vector.x
-		+ matrix.matrixData[1] * vector.x
-		+ matrix.matrixData[2] * vector.x
-		+ matrix.matrixData[3] * vector.x
-		+ matrix.matrixData[4] * vector.y
-		+ matrix.matrixData[5] * vector.y
-		+ matrix.matrixData[6] * vector.y
-		+ matrix.matrixData[7] * vector.y
-		+ matrix.matrixData[8] * vector.z
-		+ matrix.matrixData[9] * vector.z
-		+ matrix.matrixData[10] * vector.z
-		+ matrix.matrixData[11] * vector.z
-		+ matrix.matrixData[12] + matrix.matrixData[13] + matrix.matrixData[14] + matrix.matrixData[15];
+	Result.matrixData[14] = matrix.matrixData[0] * vector.x + matrix.matrixData[1] * vector.x + matrix.matrixData[2] * vector.x + matrix.matrixData[3] * vector.x + matrix.matrixData[4] * vector.y + matrix.matrixData[5] * vector.y + matrix.matrixData[6] * vector.y + matrix.matrixData[7] * vector.y + matrix.matrixData[8] * vector.z + matrix.matrixData[9] * vector.z + matrix.matrixData[10] * vector.z + matrix.matrixData[11] * vector.z + matrix.matrixData[12] + matrix.matrixData[13] + matrix.matrixData[14] + matrix.matrixData[15];
 
-
-	Result.matrixData[15] = matrix.matrixData[0] * vector.x
-		+ matrix.matrixData[1] * vector.x
-		+ matrix.matrixData[2] * vector.x
-		+ matrix.matrixData[3] * vector.x
-		+ matrix.matrixData[4] * vector.y
-		+ matrix.matrixData[5] * vector.y
-		+ matrix.matrixData[6] * vector.y
-		+ matrix.matrixData[7] * vector.y
-		+ matrix.matrixData[8] * vector.z
-		+ matrix.matrixData[9] * vector.z
-		+ matrix.matrixData[10] * vector.z
-		+ matrix.matrixData[11] * vector.z
-		+ matrix.matrixData[12] + matrix.matrixData[13] + matrix.matrixData[14] + matrix.matrixData[15];
+	Result.matrixData[15] = matrix.matrixData[0] * vector.x + matrix.matrixData[1] * vector.x + matrix.matrixData[2] * vector.x + matrix.matrixData[3] * vector.x + matrix.matrixData[4] * vector.y + matrix.matrixData[5] * vector.y + matrix.matrixData[6] * vector.y + matrix.matrixData[7] * vector.y + matrix.matrixData[8] * vector.z + matrix.matrixData[9] * vector.z + matrix.matrixData[10] * vector.z + matrix.matrixData[11] * vector.z + matrix.matrixData[12] + matrix.matrixData[13] + matrix.matrixData[14] + matrix.matrixData[15];
 }
 
 void ObjectRenderer::updateUniformBuffer(Camera camera)
@@ -165,15 +111,16 @@ void ObjectRenderer::updateUniformBuffer(Camera camera)
 
 	ubo.proj.matrixData[5] *= -1;
 
-	void* data;
+	void *data;
 
 	data = Engine::Graphics::getInstance()->getDevice()->getUniqueDevice()->get().mapMemory(objBuffers.uniformBuffersMemory, 0, sizeof(ubo));
-	
+
 	memcpy(data, &ubo, sizeof(ubo));
 	Engine::Graphics::getInstance()->getDevice()->getUniqueDevice()->get().unmapMemory(objBuffers.uniformBuffersMemory);
 }
 
-void ObjectRenderer::destroy() {
+void ObjectRenderer::destroy()
+{
 
 	//gPipeline.destroy();
 	descriptor.destroy();
